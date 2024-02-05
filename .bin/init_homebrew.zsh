1f9156c2f0a0ec91f0655d9f5c75592c120d66b5
Ryz <35394584+ryzca@users.noreply.github.com>
2024-02-05 22:53:42 +0900

    fix homebrew

--- .bin/init_homebrew.zsh
+++ .bin/init_homebrew.zsh
@@ -5,18 +5,23 @@ if [ "$(uname)" != "Darwin" ]; then
   exit 1
 fi
 
-if type brew >/dev/null; then
+if type brew &> /dev/null; then
   log "Homebrew is already installed." "info"
 else
   log "==> Installing Homebrew" "notice"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  source "${DOTFILES_CONFIGS}/zsh/.zprofile"
 fi
 
 log "==> Updating Homebrew" "notice"
 brew update
 
-log "==> Installing CLt" "notice"
-xcode-select --install
+if xcode-select --print-path &> /dev/null; then
+  log "Command Line Tools are already installed.", "info"
+else
+  log "==> Installing Command Line Tools" "notice"
+  xcode-select --install
+fi
 
 log "==> Installing Homebrew apps (formula & cask)" "notice"
 brew bundle install --file=${DOTFILES_CONFIGS}/homebrew/Brewfile --verbose --no-lock
