5de194fe3f4e814d3ecfa1b32ff73c7a090059a8
Ryz <35394584+ryzca@users.noreply.github.com>
2024-03-26 15:39:20 +0900

    modify fzf

--- configs/zsh/conf.d/fzf.zsh
+++ configs/zsh/conf.d/fzf.zsh
@@ -10,14 +10,14 @@ export FZF_DEFAULT_OPTS="
     --pointer='$(echo "\uF00c")'
     --marker='$(echo "\uF00c")'
     --color fg:248,fg+:white,bg:-1,bg+:-1,hl:red,hl+:red
-    --color preview-fg:white
+    --color preview-fg:white,preview-scrollbar:yellow
     --color gutter:-1,prompt:blue,info:yellow,pointer:red,marker:green
 "
 
 export FZF_DEFAULT_COMMAND='fd --type file'
 
 function _fzf_cdr() {
-    target_dir="$(cdr -l | awk '{ print $2 }' | fzf --preview 'f() { sh -c "eza -1A --color=always $1" }; f {}')"
+    target_dir="$(cdr -l | awk '{ print $2 }' | fzf --preview 'f() { sh -c "eza --icons --tree --level=2 --color=always $1" }; f {}')"
     target_dir="$(echo ${target_dir/\~/${HOME}})"
     if [[ -n "${target_dir}" ]]; then
         cd "${target_dir}"
@@ -27,21 +27,22 @@ function _fzf_cdr() {
 function _fzf_branch() {
   local branches branch
   branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
-  branch=$(echo "$branches" | fzf --no-multi --ansi --preview='git --no-pager log -100 --graph --oneline --decorate=full --color=always {}') &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+  branch=$(echo "${branches}" | fzf --no-multi --ansi --preview='git --no-pager log -100 --graph --oneline --decorate=full --color=always {}') &&
+  git checkout $(echo "${branch}" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
 
 function _fzf_history() {
-  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
+  print -z $( ([ -n "${ZSH_NAME}" ] && fc -l 1 || history) | fzf +s --tac | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
 }
 
 function _fzf_cd() {
   local dir
-  dir=$(fd --type d 2> /dev/null | fzf +m --preview 'f() { sh -c "eza -1A --color=always $1" }; f {}') && cd "$dir"
+  dir=$(fd --type d 2> /dev/null | fzf +m --preview 'f() { sh -c "eza --icons --tree --level=2 --color=always $1" }; f {}') && cd "${dir}"
 }
 
 function _ghq_fzf () {
-    local repo="$(ghq list | fzf --query="$LBUFFER" --preview 'full_path=$(ghq list --full-path --exact {}); echo ${full_path}; eza ${full_path}')"
+    local repo="$(ghq list | fzf --query="${LBUFFER}" --preview 'p="$(ghq list --full-path --exact {})";
+        [ -f "${p}/README.md" ] && bat --style=grid,header --color=always "${p}/README.md" || eza --icons --tree --level=2 --color=always "${p}"')"
     if [[ -n "${repo}" ]]; then
         repo="$(ghq list --full-path --exact ${repo})"
         BUFFER="cd ${repo}"
@@ -50,8 +51,7 @@ function _ghq_fzf () {
     zle clear-screen
 }
 
-
-alias fcdr="_fzf_cdr"
+alias fcr="_fzf_cdr"
 alias fbr="_fzf_branch"
 alias fh="_fzf_history"
 alias fcd="_fzf_cd"
